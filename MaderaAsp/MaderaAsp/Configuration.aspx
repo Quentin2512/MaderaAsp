23c19f6f54d415f0a9140bb5bf700e2ffe4f566a
AlexBlrt <alexandre.bleriot@viacesi.fr>
2017-11-30 16:29:48 +0100

    Ajout des fonctions CreationDevis

---
+++ MaderaAsp/MaderaAsp/Configuration.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Madera.Master" AutoEventWireup="true" CodeBehind="Configuration.aspx.cs" Inherits="MaderaAsp.Configuration" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="contenu" runat="server">
+    <h1>Configuration de la maison :</h1>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 col-sm-6 col-xs-12">
+                <asp:Label runat="server">Gamme de la maison :</asp:Label><br/>
+                <asp:DropDownList runat="server" ID="gammeMaison" CssClass="form-control"/><br/>
+                <asp:Label runat="server">Choix du modèle :</asp:Label><br/>
+                <asp:DropDownList runat="server" ID="modeleMaison" CssClass="form-control"/><br/>
+                <asp:Label runat="server">Ajout des options :</asp:Label><br/>
+                <asp:DropDownList runat="server" ID="choixOption" CssClass="form-control"/><br/>
+                <asp:Button runat="server" Text="Ajouter" CssClass="btn btn-primary"/><br/>
+                <br/>
+                <asp:ListBox runat="server" CssClass="form-control" ID="listeOptions"/><br/>
+                <br/>
+            </div>
+        </div>
+    </div>
+</asp:Content>
---
+++ MaderaAsp/MaderaAsp/Configuration.aspx.designer.cs
@@ -0,0 +1,51 @@
+﻿//------------------------------------------------------------------------------
+// <généré automatiquement>
+//     Ce code a été généré par un outil.
+//
+//     Les modifications apportées à ce fichier peuvent provoquer un comportement incorrect et seront perdues si
+//     le code est régénéré.
+// </généré automatiquement>
+//------------------------------------------------------------------------------
+
+namespace MaderaAsp {
+    
+    
+    public partial class Configuration {
+        
+        /// <summary>
+        /// Contrôle gammeMaison.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList gammeMaison;
+        
+        /// <summary>
+        /// Contrôle modeleMaison.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList modeleMaison;
+        
+        /// <summary>
+        /// Contrôle choixOption.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList choixOption;
+        
+        /// <summary>
+        /// Contrôle listeOptions.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ListBox listeOptions;
+    }
+}
