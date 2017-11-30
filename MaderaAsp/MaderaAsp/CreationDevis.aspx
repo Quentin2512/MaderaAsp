75bea698c39a0e981e668ea6867e3b2f361c0893
Robin <robin.diguerre@outlook.fr>
2017-11-30 14:29:46 +0100

    Mise en forme du formulaire de création de Devis

--- MaderaAsp/MaderaAsp/CreationDevis.aspx
+++ MaderaAsp/MaderaAsp/CreationDevis.aspx
@@ -3,4 +3,34 @@
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="contenu" runat="server">
     <h1>Création d'un devis :</h1>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 col-sm-6 col-xs-12">
+                <h2>Projet :</h2>
+                <asp:Label runat="server">Nom du projet :</asp:Label><br/>
+                <asp:TextBox runat="server" ID="nomProjet" TextMode="SingleLine" CssClass="form-control"></asp:TextBox><br/>
+                <asp:Label runat="server">Référence du projet :</asp:Label><br/>
+                <asp:TextBox runat="server" ID="refProjet" TextMode="SingleLine" CssClass="form-control"></asp:TextBox><br/>
+                <br/>
+                <asp:Button runat="server" Text="Valider" CssClass="btn btn-primary"/>
+            </div>   
+            <div class="col-md-6 col-sm-6 col-xs-12">
+                <h2>Client :</h2>
+                <asp:Label runat="server">Nom :</asp:Label><br/>
+                <asp:TextBox runat="server" ID="nomClient" TextMode="SingleLine" CssClass="form-control"></asp:TextBox><br/>
+                <asp:Label runat="server">Prenom :</asp:Label><br/>
+                <asp:TextBox runat="server" ID="prenomClient" TextMode="SingleLine" CssClass="form-control"></asp:TextBox><br/>
+                <asp:Label runat="server">Adresse :</asp:Label><br/>
+                <asp:TextBox runat="server" ID="adresseClient" TextMode="SingleLine" CssClass="form-control"></asp:TextBox><br/>
+                <asp:Label runat="server">Code postal :</asp:Label><br/>
+                <asp:TextBox runat="server" ID="postalClient" TextMode="SingleLine" CssClass="form-control"></asp:TextBox><br/>
+                <asp:Label runat="server">Ville :</asp:Label><br/>
+                <asp:TextBox runat="server" ID="villeClient" TextMode="SingleLine" CssClass="form-control"></asp:TextBox><br/>
+                <asp:Label runat="server">Mail :</asp:Label><br/>
+                <asp:TextBox runat="server" ID="mailClient" TextMode="SingleLine" CssClass="form-control"></asp:TextBox><br/>
+                <asp:Label runat="server">Telephone :</asp:Label><br/>
+                <asp:TextBox runat="server" ID="phoneClient" TextMode="SingleLine" CssClass="form-control"></asp:TextBox><br/>
+            </div>
+        </div>
+    </div>
 </asp:Content>
--- MaderaAsp/MaderaAsp/CreationDevis.aspx.designer.cs
+++ MaderaAsp/MaderaAsp/CreationDevis.aspx.designer.cs
@@ -11,5 +11,86 @@ namespace MaderaAsp {
     
     
     public partial class CreationDevis {
+        
+        /// <summary>
+        /// Contrôle nomProjet.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox nomProjet;
+        
+        /// <summary>
+        /// Contrôle refProjet.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox refProjet;
+        
+        /// <summary>
+        /// Contrôle nomClient.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox nomClient;
+        
+        /// <summary>
+        /// Contrôle prenomClient.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox prenomClient;
+        
+        /// <summary>
+        /// Contrôle adresseClient.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox adresseClient;
+        
+        /// <summary>
+        /// Contrôle postalClient.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox postalClient;
+        
+        /// <summary>
+        /// Contrôle villeClient.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox villeClient;
+        
+        /// <summary>
+        /// Contrôle mailClient.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox mailClient;
+        
+        /// <summary>
+        /// Contrôle phoneClient.
+        /// </summary>
+        /// <remarks>
+        /// Champ généré automatiquement.
+        /// Pour modifier, déplacez la déclaration de champ du fichier de concepteur dans le fichier code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox phoneClient;
     }
 }
